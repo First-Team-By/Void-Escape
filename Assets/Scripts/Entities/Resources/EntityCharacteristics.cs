e1a253c8839e8b5a8af8909323460721b2f8cbab
neosInPocket <fortpost197080@gmail.com>
2022-10-26 22:35:20 +0300

    Base entity realisation

---
+++ Assets/Scripts/Entities/Resources/EntityCharacteristics.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Entity Characteristics", menuName = "Game/Entities/Entity Characteristics")]
+public class EntityCharacteristics : ScriptableObject
+{
+    [SerializeField] private float MaxHealth;
+
+    [Range(0, 1)]
+    [SerializeField] private float CritChance;
+
+    [Range(1, 2)]
+    [SerializeField] private float CritMultiplier;
+
+    [Range(0,1)]
+    [SerializeField] private float EvadeChance;
+
+    [Range(0, 1)]
+    [SerializeField] private float Accuracy;
+
+    [Range(0, 0.8f)] 
+    [SerializeField] private float Defence;
+
+    public float _maxHealth => MaxHealth;
+    public float _critChance => CritChance;
+    public float _critMultiplier => CritMultiplier;
+    public float _evadeChance => EvadeChance;
+    public float _accuracy => _accuracy;
+    public float _defence => Defence;
+}
