48846da2134f93d79d0e3cec14b02041beb8c696
neosInPocket <fortpost197080@gmail.com>
2022-10-29 23:33:22 +0300

    Battle routine changes

--- Assets/BattleRoutine.cs
+++ Assets/BattleRoutine.cs
@@ -1,41 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.ExceptionServices;
 using UnityEngine;
 
 public class BattleRoutine : MonoBehaviour
 {
-    [SerializeField] private GameObject CharacterPositions;
-    [SerializeField] private GameObject EnemyPositions;
+    public List<Character> characters => gameObject.GetComponentsInChildren<Character>().ToList();
+    public List<Enemy> enemies => gameObject.GetComponentsInChildren<Enemy>().ToList();
 
-    private Dictionary<Character, int> characters;
-    private Dictionary<Character, int> enemies;
-    public void Init(Dictionary<Character, int> characters, Dictionary<Character, int> enemies)
+    private Queue<EntityBase> turnQueue;
+
+    public int roundCounter { get; private set; }
+
+    private void Init()
     {
-        this.characters = characters;
-        this.enemies = enemies;
-
-        Transform[] charactersPositions = CharacterPositions.gameObject.GetComponentsInChildren<Transform>();
-        Transform[] enemiesPositions = EnemyPositions.gameObject.GetComponentsInChildren<Transform>();
+        roundCounter = 1;
+        turnQueue = new Queue<EntityBase>();
     }
 
-    public void EntitySelect(EntityBase entity)
+    private void InitQueue()
     {
-
-    }
-
-    public void SwapEntitiesPosition(EntityBase entity1, EntityBase entity2)
-    {
-
-    }
-
-    private void SetEntities(GameObject position, Dictionary<EntityBase, int> entities)
-    {
-        Transform[] positions = position.GetComponentsInChildren<Transform>();
-
-        for (int i = 0; i < entities.Count; i++)
+        foreach (var character in characters)
         {
-            
+            turnQueue.Enqueue(character);
         }
+
+        foreach (var enemy in enemies)
+        {
+            turnQueue.Enqueue(enemy);
+        }
+    }
+
+    private void EntityTurn()
+    {
+        if (turnQueue.Count == 0)
+        {
+            InitQueue();
+        }
+    }
+
+    void Start()
+    {
+        Init();
+
+
     }
 }
