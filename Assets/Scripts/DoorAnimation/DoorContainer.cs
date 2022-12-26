3f09b0b7520df397928b05a71ada8855ea3ca834
AliaksandrParetski <aliaksandrparetski@gmail.com>
2022-12-26 20:36:57 +0300

    Анимация
    Анимация дверей

---
+++ Assets/Scripts/DoorAnimation/DoorContainer.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorContainer : MonoBehaviour
+{
+    [SerializeField] private GameObject _topDoor;
+
+    [SerializeField] private GameObject _bottomDoor;
+
+    [SerializeField] private GameObject _door;
+
+    private SpriteRenderer _topDoorSR;
+
+    private SpriteRenderer _bottomDoorSR;
+
+    private Animator _anim;
+
+
+
+    private void Start()
+    {
+        _topDoorSR = _topDoor.GetComponent<SpriteRenderer>();
+        _bottomDoorSR = _bottomDoor.GetComponent<SpriteRenderer>();
+        _anim = _door.GetComponent<Animator>();
+    }
+
+    private void OnMouseEnter()
+    {
+        _topDoorSR.color = Color.gray;
+        _bottomDoorSR.color = Color.gray;
+    }
+
+    private void OnMouseExit()
+    {
+        _topDoorSR.color = Color.white;
+        _bottomDoorSR.color = Color.white;
+
+        _anim.SetBool("isOpen", false);
+    }
+
+    private void OnMouseDown()
+    {
+        _anim.SetBool("isOpen", true);
+    }
+}
